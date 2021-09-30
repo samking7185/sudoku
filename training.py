ffc9a7ebfc334a9a4378de53f0e85f1d1af5049e
samking7185 <47400683+samking7185@users.noreply.github.com>
2021-09-30 13:39:17 -0400

    Started fuzzy recognition

--- training.py
+++ training.py
@@ -1,4 +1,3 @@
-from image_NN import Network
 import numpy as np
 import load_mnist
 
@@ -7,33 +6,16 @@ training_data = list(training_data)
 test_data = list(test_data)
 validation_data = list(validation_data)
 
-net = Network([784, 60, 30, 10])
-
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
-
-np.savetxt('Biases1.txt', net.biases[0])
-np.savetxt('Biases2.txt', net.biases[1])
-np.savetxt('Biases3.txt', net.biases[2])
-
-np.savetxt('Weights1.txt', net.weights[0])
-np.savetxt('Weights2.txt', net.weights[1])
-np.savetxt('Weights3.txt', net.weights[2])
-
-# textfile = open("results.txt", "w")
-# iteration = 1
+# This is the training for the neural network
+# net = Network([784, 60, 30, 10])
 #
-# for layer in net.biases:
-#     sec_title = "________ Bias Layer %d ________" % iteration
-#     textfile.write(sec_title + "\n")
-#     iteration += 1
-#     for neuron in layer:
-#         textfile.write(str(neuron) + "\n")
+# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 #
-# iteration = 1
-# for layer in net.weights:
-#     sec_title = "________ Weights Layer %d ________" % iteration
-#     textfile.write(sec_title + "\n")
-#     iteration += 1
-#     for neuron in layer:
-#         textfile.write(str(neuron) + "\n")
-# textfile.close()
+# np.savetxt('Biases1.txt', net.biases[0])
+# np.savetxt('Biases2.txt', net.biases[1])
+# np.savetxt('Biases3.txt', net.biases[2])
+#
+# np.savetxt('Weights1.txt', net.weights[0])
+# np.savetxt('Weights2.txt', net.weights[1])
+# np.savetxt('Weights3.txt', net.weights[2])
+
