60a74d4c8e3481eb02f5a05c7ee95f844123f855
samking7185 <47400683+samking7185@users.noreply.github.com>
2021-10-07 11:09:34 -0400

    Added silhouette calculation

--- cluster.py
+++ cluster.py
@@ -17,43 +17,66 @@ BW = cv2.threshold(new_image, 0.6, 1, cv2.THRESH_BINARY)
 BW = np.array(BW[1])
 bigdata = np.nonzero(BW)
 bigdata = np.stack(bigdata, axis=1)
+n = len(bigdata)
+J = []
+H = []
+maxiter = 100
 
-k = 8
-q = 2
-error = True
-error_val = 0.00001
-power = -2 / (q - 1)
+for k in range(3, 10, 1):
+    # k = 4
+    q = 2
+    error = True
+    error_val = 0.00001
+    power = -2 / (q - 1)
 
-Mu_last = np.zeros((len(bigdata), k))
-Dmat = np.zeros((bigdata.shape[0], k))
+    Mu_last = np.zeros((n, k))
+    Dmat = np.zeros((bigdata.shape[0], k))
 
-Mu = np.random.rand(k, len(bigdata))
-Mu /= np.ones((k, 1)).dot(np.atleast_2d(Mu.sum(axis=0))).astype(np.float64)
-Mu = np.fmax(Mu, np.finfo(np.float64).eps)
-Mu = Mu.T
-
-while error:
-    Vhsq = np.power(Mu, q)
-    Vh = np.zeros((k, 2))
-
-    for idx in range(k):
-        Vh[idx,:] = np.sum(np.transpose(np.tile(Vhsq[:,idx], (2,1))) * bigdata, axis=0) / np.sum(Vhsq[:,idx])
-    for idx in range(k):
-        Dmat[:, idx] = np.linalg.norm(bigdata - Vh[idx, :], axis=1)
-
-    Dmat = np.fmax(Dmat, np.finfo(np.float64).eps)
-    J = np.sum(np.power(np.multiply(Dmat, Mu), 2))
-    Mu = np.power(Dmat, power).T
+    Mu = np.random.rand(k, n)
     Mu /= np.ones((k, 1)).dot(np.atleast_2d(Mu.sum(axis=0))).astype(np.float64)
-
-    if np.absolute(np.linalg.norm(Mu.T - Mu_last)) < error_val:
-        error = False
+    Mu = np.fmax(Mu, np.finfo(np.float64).eps)
     Mu = Mu.T
-    Mu_last = Mu
+
+    for iteration in range(maxiter):
+        Vhsq = np.power(Mu, q)
+        Vh = np.zeros((k, 2))
+
+        for idx in range(k):
+            Vh[idx,:] = np.sum(np.transpose(np.tile(Vhsq[:,idx], (2,1))) * bigdata, axis=0) / np.sum(Vhsq[:,idx])
+        for idx in range(k):
+            Dmat[:, idx] = np.linalg.norm(bigdata - Vh[idx, :], axis=1)
+
+        Dmat = np.fmax(Dmat, np.finfo(np.float64).eps)
+        Mu = np.power(Dmat, power).T
+        Mu /= np.ones((k, 1)).dot(np.atleast_2d(Mu.sum(axis=0))).astype(np.float64)
+
+        if np.absolute(np.linalg.norm(Mu.T - Mu_last)) < error_val:
+            Mu = Mu.T
+            break
+        Mu = Mu.T
+        Mu_last = Mu
+
+index_vals = np.argmax(Mu, axis=1)
+cluster_assign = np.empty(k, dtype=object)
+cluster_dist_bar = np.empty(k, dtype=object)
+for idx in range(k):
+    cluster_assign[idx] = bigdata[index_vals == idx]
+    cluster_dist = np.array([np.linalg.norm(cluster_assign[idx] - x, axis=1) for x in cluster_assign[idx]])
+    cluster_dist_bar[idx] = np.mean(cluster_dist, axis=1)
+cluster_score = np.empty(k, dtype=object)
+
+for j,(cls,bar) in enumerate(zip(cluster_assign, cluster_dist_bar)):
+    cluster_score[j] = np.zeros(len(cls))
+    for i,(x,m) in enumerate(zip(cls, bar)):
+        cls_dist = [np.mean(np.linalg.norm(y - x)) for y in cluster_assign if not np.array_equal(y,cls)]
+        cls_dbar = np.mean(cls_dist)
+        cluster_score[j][i] = 1 - m / cls_dbar
+
+s = 1
 
 # print(C)
-plt.scatter(bigdata[:,0], bigdata[:,1])
-plt.scatter(Vh[:,0], Vh[:,1])
-plt.show()
+# plt.scatter(bigdata[:,0], bigdata[:,1])
+# plt.scatter(Vh[:,0], Vh[:,1])
+# plt.show()
 
 
